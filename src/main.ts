f40706e3cdacd06abc7c1bee6e1c64d73b7519d2
Paxton Terry <paxtonterrydev@gmail.com>
2026-01-20 12:38:44 -0700

    Defines basic testing structure for main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,46 @@
-console.log("Hello, World!");
+import type { Model } from "./lib/model";
+import { getPermissions } from "./lib/permission";
+
+interface Address {
+  street: string;
+  city: string;
+  state: string;
+}
+
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: Address;
+}
+
+const userResponse: Model<User> = {
+  firstName: {
+    value: "John",
+    permissions: ["Read", "Update"],
+  },
+  lastName: {
+    value: "Smith",
+    permissions: ["Read", "Update"],
+  },
+  age: {
+    value: 32,
+    permissions: ["Read"],
+  },
+  address: {
+    street: {
+      value: "123 Main Street",
+      permissions: ["Read", "Update"],
+    },
+    city: {
+      value: "Smallville",
+      permissions: ["Read"],
+    },
+    state: {
+      value: "Utah",
+      permissions: ["Read"],
+    },
+  },
+};
+
+
