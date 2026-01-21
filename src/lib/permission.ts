2cd6e2b8240c1ef974431e2c520b89431b141001
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2026-01-21 12:10:52 -0700

    Creates PermissionSet class with basic operations

--- src/lib/permission.ts
+++ src/lib/permission.ts
@@ -1,9 +1,71 @@
 export type Permission = "Create" | "Read" | "Update" | "Delete";
 
-export function getPermissions<T extends { permissions: Permission[] }>(prop: T): Permission[] {
-  return prop.permissions;
-} 
+type CRUDShorthand = `${"C" | ""}${"R" | ""}${"U" | ""}${"D" | ""}`
 
-export class Action {}
-export class ActionSet {}
-export class PermissionSet {}
+function parseShorthand(sh: CRUDShorthand): Permission[] {
+  const p: Permission[] = [];
+  const map = {
+    "C": "Create",
+    "R": "Read",
+    "U": "Update",
+    "D": "Delete"
+  } as const;
+
+  for (const char of sh) {
+    p.push(map[char as keyof typeof map]);
+  }
+  return p;
+}
+
+export class PermissionSet {
+  private _permissions = new Set<Permission>;
+  get permissions() {
+    return this._permissions
+  }
+
+  constructor(initialPermissions: Permission[]);
+  constructor(initialPermissions: CRUDShorthand);
+  constructor(initialPermissions: Permission[] | CRUDShorthand) {
+    let permissions: Permission[];
+    if (Array.isArray(initialPermissions)) permissions = initialPermissions;
+    else permissions = parseShorthand(initialPermissions);
+
+    for (const permission of permissions) {
+      this._permissions.add(permission)
+    }
+  }
+
+  add(permission: Permission): void;
+  add(permission: Permission[]): void;
+  add(permission: Permission | Permission[]) {
+    if (Array.isArray(permission)) {
+      for (const p of permission) {
+        this._permissions.add(p);
+      }
+    }
+
+    else this._permissions.add(permission);
+  }
+
+  has(permission: Permission) {
+    this._permissions.has(permission);
+  }
+
+  hasAny(permission: Permission[]): boolean {
+    if (Array.isArray(permission)) {
+      for (const p of permission) {
+        if (!this._permissions.has(p)) return true
+      }
+    }
+    return false;
+  }
+
+  hasAll(permission: Permission[]): boolean {
+    if (Array.isArray(permission)) {
+      for (const p of permission) {
+        if (!this._permissions.has(p)) return false
+      }
+    }
+    return true;
+  }
+}
